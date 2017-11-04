24cebb693d2538d4f87f21c15d094ef741c027e3
Rebecca Holliday <rebeccaimac@rebeccas-imac.lan>
2017-11-04 15:55:28 -0700

    Removed hardcoded column names from functions.

--- preprocessing.py
+++ preprocessing.py
@@ -28,28 +28,23 @@ class preprocess(object):
         extra white space, and removing english stop words.
         
         params:
-            data: a panda dataframe with columns 'body' and 'title' containing
-            text.
+            data: a column in a panda dataframe.
         
         output:
-            data: a panda dataframe with clean columns 'body' and 'title.'
+            data: a panda dataframe with the new clean column.
         """
         
         # Make text lowercase
         if (self.make_lowercase):
-            data['body'] = data['body'].str.lower()
-            data['title'] = data['title'].str.lower()
+            data = data.str.lower()
         
         # Remove punctuation
         if (self.rm_punctuation):
-            data['body'] = data['body'].str.replace('[^\w\s]','')
-            data['title'] = data['title'].str.replace(
-                    '[^\w\s]','')
+            data= data.str.replace('[^\w\s]','')
         
         # Remove extra whitespace
         if (self.rm_whitespace):
-            data['body'] = data['body'].str.strip()
-            data['title'] = data['title'].str.strip()
+            data = data.str.strip()
         
         # Remove stop words
         if (self.rm_stopwords):
@@ -63,11 +58,7 @@ class preprocess(object):
                             )
             
             # Remove stop words from the body column
-            data['body'] = data['body'].apply(stop_word_func)
-                    
-                    
-            # Remove stop words from the title column
-            data['title'] = data['title'].apply(stop_word_func)
+            data = data.apply(stop_word_func)
         
         return data
 
@@ -103,7 +94,8 @@ class preprocess(object):
 # Testing below. Delete before merging.
 jokeData = pd.read_json("reddit_jokes.json")
 prep = preprocess()       
-jokeData = prep.cleanData(jokeData)
+jokeData['body'] = prep.cleanData(jokeData['body'])
+jokeData['title'] = prep.cleanData(jokeData['title'])
 words = prep.tokenizeText(jokeData['title'])
 print(words[1])
 print(jokeData.head(n=5))
